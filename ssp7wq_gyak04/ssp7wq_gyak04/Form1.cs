b3605bdfb92c0df9672fc198f5ab9195b585ba74
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-10-03 14:26:14 +0200

    6

--- ssp7wq_gyak04/ssp7wq_gyak04/Form1.cs
+++ ssp7wq_gyak04/ssp7wq_gyak04/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace ssp7wq_gyak04
 {
@@ -17,6 +18,9 @@ namespace ssp7wq_gyak04
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
@@ -26,6 +30,31 @@ namespace ssp7wq_gyak04
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
