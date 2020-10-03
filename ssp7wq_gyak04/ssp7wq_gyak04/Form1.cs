9198247b91defdc4d128837e3645b2e79d71bba7
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-10-03 16:00:12 +0200

    7

--- ssp7wq_gyak04/ssp7wq_gyak04/Form1.cs
+++ ssp7wq_gyak04/ssp7wq_gyak04/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 using System.Data.Entity.Migrations.Model;
+using System.Drawing.Text;
 
 namespace ssp7wq_gyak04
 {
@@ -40,7 +41,7 @@ namespace ssp7wq_gyak04
                 xlWB = xlApp.Workbooks.Add(Missing.Value);
                 xlSheet = xlWB.ActiveSheet;
 
-                //CreateTable();
+                CreateTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
@@ -55,6 +56,64 @@ namespace ssp7wq_gyak04
                 xlWB = null;
                 xlApp = null;
             }
+
+        
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] 
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            int length = headers.Length;
+            for (int i = 0; i < length; i++)
+            {
+                xlSheet.Cells[1, length + 1] = headers[length];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int szamlalo = 0;
+            foreach (Flat f in Flats)
+            {
+                values[szamlalo, 0]=f.Code;
+                values[szamlalo, 1] = f.Code;
+                values[szamlalo, 2] = f.Code;
+                values[szamlalo, 3] = f.Code;
+                values[szamlalo, 4] = f.Code;
+                values[szamlalo, 5] = f.Code;
+                values[szamlalo, 6] = f.Code;
+                values[szamlalo, 7] = f.Code;
+                values[szamlalo, 8] = "";
+                szamlalo++;
+            }
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
