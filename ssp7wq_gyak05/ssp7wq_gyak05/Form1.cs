c09ca952a1afc1b965f6bb7ef7607c33c1be262e
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-10-11 13:53:55 +0200

    7

--- ssp7wq_gyak05/ssp7wq_gyak05/Form1.cs
+++ ssp7wq_gyak05/ssp7wq_gyak05/Form1.cs
@@ -33,5 +33,19 @@ namespace ssp7wq_gyak05
 
             dataGridView2.DataSource = Portfolio;
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                              && date <= x.TradingDay
+                            select x).First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
