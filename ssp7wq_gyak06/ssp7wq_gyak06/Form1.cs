77a8b0c817470a2081c73de7d2c3cbe3b00b8083
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-10-17 17:32:47 +0200

    5

--- ssp7wq_gyak06/ssp7wq_gyak06/Form1.cs
+++ ssp7wq_gyak06/ssp7wq_gyak06/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ssp7wq_gyak06
 {
@@ -22,7 +23,7 @@ namespace ssp7wq_gyak06
             dataGridView1.DataSource = Rates;
         }
 
-        private void start()
+        public void start()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
@@ -33,6 +34,28 @@ namespace ssp7wq_gyak06
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(item.GetAttribute("date"));
+
+                var childElement = (XmlElement)item.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit!=0)
+                {
+                    rate.Value = value / unit;
+                }
+
+            }
         }
     }
 }
