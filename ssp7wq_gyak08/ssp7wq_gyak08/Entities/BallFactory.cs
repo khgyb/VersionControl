23df998115912b5ba7a360598b7857fd38eea76e
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-11-12 19:24:41 +0100

    3

---
+++ ssp7wq_gyak08/ssp7wq_gyak08/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ssp7wq_gyak08.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
