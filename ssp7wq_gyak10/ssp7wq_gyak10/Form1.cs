44d29e5edef6016bdf282991701deaeb50d76b24
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-11-28 19:31:19 +0100

    3

--- ssp7wq_gyak10/ssp7wq_gyak10/Form1.cs
+++ ssp7wq_gyak10/ssp7wq_gyak10/Form1.cs
@@ -21,6 +21,9 @@ namespace ssp7wq_gyak10
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            //gc.AddPlayer();
+            //gc.Start(true);
         }
     }
 }
