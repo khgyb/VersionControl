ce00a8317c856f82b0968bcad6f43248aac55cb3
Benedek Köröshegyi <bkoroshegyi@live.com>
2020-11-28 19:49:44 +0100

    7

--- ssp7wq_gyak10/ssp7wq_gyak10/Form1.cs
+++ ssp7wq_gyak10/ssp7wq_gyak10/Form1.cs
@@ -20,6 +20,8 @@ namespace ssp7wq_gyak10
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+
+        Brain winnerBrain = null;
         public Form1()
         {
             InitializeComponent();
@@ -48,6 +50,16 @@ namespace ssp7wq_gyak10
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
 
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
